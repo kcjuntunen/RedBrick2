096b96eb28751b0856eef18581f9f5325a045b46
KC Juntunen <juntunenkc@lan.amstore.com>
2018-01-03 08:53:55 -0500

    Update ArchivePDF, do gauge check in drawing.

--- RedBrick2/DrawingRedbrick.cs
+++ RedBrick2/DrawingRedbrick.cs
@@ -293,6 +293,9 @@ namespace RedBrick2 {
 		/// Write data to properties.
 		/// </summary>
 		public void Commit() {
+			ArchivePDF.csproj.ArchivePDFWrapper aw_ =
+				new ArchivePDF.csproj.ArchivePDFWrapper(SwApp, Redbrick.GeneratePathSet());
+			aw_.CheckGauges();
 			PropertySet[@"CUSTOMER"].Data = cust_cbx.SelectedValue;
 			PropertySet[@"REVISION LEVEL"].Data = rev_cbx.Text;
 			PropertySet[@"DrawnBy"].Data = auth_cpx.SelectedValue;
