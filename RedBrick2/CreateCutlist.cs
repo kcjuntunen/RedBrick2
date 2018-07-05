bfb6696902f32849ffb0f38f04cab495ed53894c
KC Juntunen <juntunenkc@lan.amstore.com>
2018-07-05 07:36:07 -0400

    Not needed.

--- RedBrick2/CreateCutlist.cs
+++ RedBrick2/CreateCutlist.cs
@@ -41,7 +41,6 @@ namespace RedBrick2 {
 																			 false, false, false, false, false, false,
 																			 false, false, false, false, false, false };
 		private bool ok = false;
-		private bool data_from_db_ = false;
 		private string itm = string.Empty;
 		private string descr = string.Empty;
 		private string refr = string.Empty;
@@ -1033,9 +1032,6 @@ namespace RedBrick2 {
 				DataGridViewComboBoxCell er_ = grid_.Rows[e.RowIndex].Cells[@"er"] as DataGridViewComboBoxCell;
 				DataGridViewComboBoxCell el_ = grid_.Rows[e.RowIndex].Cells[@"el"] as DataGridViewComboBoxCell;
 
-				if (grid_.Columns[e.ColumnIndex].Name.StartsWith(@"Op")) {
-					data_from_db_ = false;
-				}
 				//object ds_ = (grid_.Rows[e.RowIndex].Cells[@"Op 1"] as DataGridViewComboBoxCell).DataSource;
 				//if (ds_ is DataTable && dpt_cell_.Value != null) {
 				//	object ds_filtered_ = (ds_ as DataTable).Select(string.Format(@"TYPEID = {0}", dpt_cell_.Value));
@@ -1650,7 +1646,6 @@ namespace RedBrick2 {
 		}
 
 		private void scan_Click(object sender, EventArgs e) {
-			data_from_db_ = false;
 			if (parts_rdo.Checked && mDoc is AssemblyDoc) {
 				GetAssembly(mDoc);
 			} else if (toplvl_rdo.Checked) {
@@ -1690,7 +1685,6 @@ namespace RedBrick2 {
 
 		private void getdatafromDBbtn_Click_1(object sender, EventArgs e) {
 			if (MessageBox.Show(@"Read part data from DB?", @"﴾͡๏̯͡๏﴿ RLY?", MessageBoxButtons.YesNo) == DialogResult.Yes) {
-				data_from_db_ = true;
 				ReadGlobalFromDb();
 			}
 		}
