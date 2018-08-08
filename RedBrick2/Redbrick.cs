ab64a01271a22d9e5ecf1f29339a5da4104da088
KC Juntunen <juntunenkc@lan.amstore.com>
2018-08-08 08:09:24 -0400

    Comment

--- RedBrick2/Redbrick.cs
+++ RedBrick2/Redbrick.cs
@@ -24,6 +24,10 @@ namespace RedBrick2 {
 		private Version currentVersion;
 		private bool askedToUpdate = false;
 		private string UpdateMessage = string.Empty;
+
+		/// <summary>
+		/// A place to store the last ECR description that was typed.
+		/// </summary>
 		static public string LastECRDescription = string.Empty;
 		/// <summary>
 		/// Translation table of ugly field names to pretty ones.
