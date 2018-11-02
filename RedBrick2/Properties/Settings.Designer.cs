459a4f20a163aeb36d2818857df95b01bc404645
KC Juntunen <juntunenkc@lan.amstore.com>
2018-11-02 13:05:02 -0400

    New server.

--- RedBrick2/Properties/Settings.Designer.cs
+++ RedBrick2/Properties/Settings.Designer.cs
@@ -50,7 +50,7 @@ namespace RedBrick2.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=AMSTORE-SQL-05;Initial Catalog=ENGINEERING;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=AMSTORE-SQL-07;Initial Catalog=ENGINEERING;Integrated Security=True")]
         public string ENGINEERINGConnectionString {
             get {
                 return ((string)(this["ENGINEERINGConnectionString"]));
