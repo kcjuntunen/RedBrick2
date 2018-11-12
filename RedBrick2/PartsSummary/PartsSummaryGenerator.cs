794a5c12893bba4ae152343f26e5906d6295e666
KC Juntunen <juntunenkc@lan.amstore.com>
2018-11-12 15:17:35 -0500

    This goes with e780547. I gotta stop doing that.

---
+++ RedBrick2/PartsSummary/PartsSummaryGenerator.cs
@@ -0,0 +1,58 @@
+﻿using OfficeOpenXml;
+
+namespace RedBrick2.PartsSummary {
+	public class PartsSummaryGenerator {
+		private System.Collections.Specialized.OrderedDictionary _partsList;
+		private string _path;
+
+		public PartsSummaryGenerator(System.Collections.Specialized.OrderedDictionary partsList, string path) {
+			_partsList = partsList;
+			_path = path;
+		}
+
+		public void Generate() {
+			using (ExcelPackage xlp_ = new ExcelPackage(new System.IO.FileInfo(_path))) {
+				ExcelWorksheet wksht_ = xlp_.Workbook.Worksheets[1];
+				int max_row = wksht_.Dimension.End.Row - 2;
+				int row_offset = 2;
+				int row = 1 + row_offset;
+				foreach (string key in _partsList.Keys) {
+					SwProperties p = (SwProperties)_partsList[key];
+					wksht_.Cells[row, 2].Value = p.PartLookup;
+					wksht_.Cells[row, 3].Value = p[@"Description"].ResolvedValue;
+					wksht_.Cells[row, 4].Value = p.CutlistQty;
+					wksht_.Cells[row, 5].Value = p[@"CUTLIST MATERIAL"].ResolvedValue;
+					wksht_.Cells[row, 6].Value = p[@"LENGTH"].Data;
+					wksht_.Cells[row, 7].Value = p[@"WIDTH"].Data;
+					wksht_.Cells[row, 8].Value = p[@"THICKNESS"].Data;
+					wksht_.Cells[row, 9].Value = p[@"BLANK QTY"].Data;
+					wksht_.Cells[row, 10].Value = p[@"OVERL"].Data;
+					wksht_.Cells[row, 11].Value = p[@"OVERW"].Data;
+					wksht_.Cells[row, 12].Value = p[@"CNC1"].ResolvedValue;
+					wksht_.Cells[row, 13].Value = p[@"CNC2"].ResolvedValue;
+					for (int i = 0; i < Properties.Settings.Default.OpCount; i++) {
+						string op_ = string.Format(@"OP{0}", i + 1);
+						wksht_.Cells[row, 14 + i].Value = p[op_].ResolvedValue;
+					}
+					if (++row >= max_row) {
+						break;
+					}
+				}
+				using (System.Windows.Forms.SaveFileDialog sfd = new System.Windows.Forms.SaveFileDialog()) {
+					sfd.Filter = @"Excel File (*.xlsx)|*.xlsx";
+					if (SuggestedName.Trim() != string.Empty) {
+						sfd.FileName = SuggestedName;
+					}
+					if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+						System.IO.FileInfo saveas = new System.IO.FileInfo(sfd.FileName);
+						xlp_.SaveAs(saveas);
+						System.Diagnostics.Process.Start(saveas.FullName);
+					}
+				}
+			}
+		}
+
+		public string SuggestedName { get; set; }
+
+	}
+}
